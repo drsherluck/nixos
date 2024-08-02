e1ae01cc3e7e8ca47c8a59038f277e6394f0dc1b
drsherluck <danilobett@gmail.com>
2024-08-02 14:51:35 +0200

    update gobar and oisd block lists

--- pkgs/gobar.nix
+++ pkgs/gobar.nix
@@ -5,8 +5,8 @@ pkgs.buildGoModule {
   src = pkgs.fetchFromGitHub {
     owner = "drsherluck";
     repo = "gobar";
-    rev = "09b79b21ad7beca937830a688b6e6019b917a38e";
-    hash = "sha256-rrwkQNTthPUrSR2eP3DAU8oRptBFLC0EIpQwMXXpW00=";
+    rev = "29a79b1a1dc761835157b963d113f5fbd95d22e9";
+    hash = "sha256-lebQojpdz0eswONC/9AIf2Z7ewg5bQZfwnTsONQtyvU=";
   };
 
   vendorHash = "sha256-qFdaH9A8m7oPKOvXWkIrseEbx8Ht8aFqB1lHA75X5fw=";
