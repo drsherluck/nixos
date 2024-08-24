fb24f253d4233dc5531435ddea7fb45518885b23
drsherluck <danilobett@gmail.com>
2024-08-24 13:28:49 +0200

    change dunst corner radius

--- home/dunst/default.nix
+++ home/dunst/default.nix
@@ -5,5 +5,12 @@
       enable = true;
       flavor = "macchiato";
     };
+    settings = {
+      global = {
+        history_length = 50;
+        corner_radius = 5;
+        frame_width = 2;
+      };
+    };
   };
 }
