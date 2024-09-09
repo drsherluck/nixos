c0e7fd306dece89bb3f55c723e06368ea9c75074
drsherluck <danilobett@gmail.com>
2024-09-09 09:33:16 +0200

    caladan autorandr configurations

--- home/work.nix
+++ home/work.nix
@@ -53,6 +53,18 @@
           product = "ZOWIE XL LCD";
           serial = "D7H05227SL0";
         };
+        Work-HDMI-0 = {
+          product = "PL2730Q";
+          serial = "1153890121602";
+        };
+        Work-DisplayPort-0 = {
+          product = "PL2730Q";
+          serial = "1153895021478";
+        };
+        Work-HDMI-1 = {
+          product = "PL2730Q";
+          serial = "1153895021477";
+        };
       };
       layout.default.config = {
         eDP = {
@@ -85,6 +97,57 @@
           x = 0;
           y = 0;
           primary = true;
+        };
+      };
+      layout.work_0.config = {
+        eDP = {
+          width = 1920;
+          height = 1080;
+          x = 1280;
+          y = 1440;
+        };
+        Work-DisplayPort-0 = {
+          width = 2560;
+          height = 1440;
+          x = 0;
+          y = 0;
+        };
+        Work-HDMI-0 = {
+          width = 2560;
+          height = 1440;
+          x = 2560;
+          y = 0;
+          primary = true;
+        };
+      };
+      layout.work_1.config = {
+        eDP = {
+          width = 1920;
+          height = 1080;
+          x = 2560;
+          y = 0;
+          primary = true;
+        };
+        Work-HDMI-1 = {
+          width = 2560;
+          height = 1440;
+          x = 0;
+          y = 0;
+        };
+      };
+      layout.work_2.config = {
+        eDP = {
+          width = 1920;
+          height = 1080;
+          x = 2560;
+          y = 0;
+          primary = true;
+        };
+        Work-DisplayPort-0 = {
+          width = 2560;
+          height = 1440;
+          x = 0;
+          y = 0;
         };
       };
     };
